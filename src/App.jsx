3d9c68c7eb457e05d604a7895b198a1861c95357
shivam kushwah <97247055+Shivamkus@users.noreply.github.com>
2024-08-28 13:36:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,8 @@ const App = () => {
   const [cityName, setCityName] = useState("khandwa");
   const [inputCity, setInputCity] = useState("");
   const date = new Date().toLocaleTimeString();
-   const DateToday = new Date().toLocaleDateString();
 
   const [time, setTime] = useState(date);
-  const [todaydate, setTodayDate] = useState(DateToday);
 
   useEffect(() => {
     fetch(
@@ -18,12 +16,11 @@ const App = () => {
       .then((res) => res.json())
       .then((data) => {
         setData(data);
-        setInputCity('')
         console.log(data);
-        
       });
   }, [cityName]);
 
+  console.log(time);
 
   const newTime = () => {
     const date = new Date().toLocaleTimeString();
@@ -31,13 +28,6 @@ const App = () => {
   };
   setInterval(newTime, 1000);
 
-  const newTodayDate = ()=>{
-    const DateToday = new Date().toLocaleDateString();
-  setTodayDate(DateToday)
-  }
-  setInterval(newTodayDate, 1000*60*60)
-
-  
   const handleInputChange = (e) => {
     setInputCity(e.target.value);
   };
@@ -52,15 +42,13 @@ const App = () => {
   }
 
   const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-  const sunriseTime = new Date(data.sys.sunrise * 1000);
-  const sunsetTime = new Date(data.sys.sunset * 1000);
+
   return (
     <>
       <div
         className="h-screen w-screen flex flex-col lg:flex-row justify-center items-center
-       bg-[url('https://images.discerningassets.com/image/upload/c_fill,h_1000,w_1000/c_fit,fl_relative,h_1.0,o_100,w_1.0/c_fill,w_4383,h_4383/v1529088444/20180608-4W6A4325_6_7-2-Edit_b8jl00.jpg')]"
-      >
-        <div
+        bg-cover bg-[url('https://images.discerningassets.com/image/upload/c_fill,h_1000,w_1000/c_fit,fl_relative,h_1.0,o_100,w_1.0/c_fill,w_4383,h_4383/v1529088444/20180608-4W6A4325_6_7-2-Edit_b8jl00.jpg')]"
+      > <div
           className="h-[30vh] lg:h-[70vh] w-[90%] lg:w-[25%] flex items-center justify-center shadow-2xl shadow-white p-2 m-2
           bg-slate-700/60 "
         >
@@ -72,49 +60,40 @@ const App = () => {
          <h1 className="text-4xl text-white flex justify-end items-end">  {time}</h1>
          </div>
 
-        </div>
-
         <div
           className="h-[50vh] lg:h-[70vh] w-[90%] lg:w-[45%] border-emerald-950 p-2 m-2 flex flex-col justify-center items-center
-          lg:bg-slate-700/80 md:bg-slate-400/50 sm:bg-slate-400/50 bg-slate-900/50 shadow-xl shadow-white"
+          bg-cover bg-blend-color-burn
+          bg-[url('https://images.discerningassets.com/image/upload/c_fill,h_1000,w_1000/c_fit,fl_relative,h_1.0,o_100,w_1.0/c_fill,w_4383,h_4383/v1529088444/20180608-4W6A4325_6_7-2-Edit_b8jl00.jpg')]
+          backdrop-blur-lg"
         >
-          <div className=" justify-center items-center">
           <div className="flex justify-center items-center">
             <img
               src={iconUrl}
               alt="Icon of Weather"
-              className="w-[50px] lg:w-[100px] bg-neutral-800/30 rounded-full"
+              className="w-[50px] lg:w-[100px] bg-neutral-800 rounded-full"
             />
           </div>
-          <div className=" w-[full] lg:text-right h-2/3 items-end justify-center flex flex-col md:text-right">
+          <div className=" w-[full] lg:text-right md:text-right">
             <h1 className="text-2xl lg:text-4xl text-white">
               {Number(data.main.temp - 273.15).toFixed(2)}°C
             </h1>
-            <h1 className="text-white text-xl lg:text-2xl">
+            <h1 className="text-white text-md lg:text-xl">
               Clouds: {data.clouds.all}%
             </h1>
-            <h1 className="text-white text-xl lg:text-2xl">
-            Sunrise:{ sunriseTime.toLocaleTimeString()}
+            <h1 className="text-white text-md lg:text-xl">
+              Air Pressure: {data.main.pressure} hPa
             </h1>
-
-            <h1 className="text-white text-lg lg:text-2xl">
-            Sunset: {sunsetTime.toLocaleTimeString()} 
-          </h1>
-
-          <h1 className="text-white text-xl lg:text-2xl">
-          Air Pressure: {data.main.pressure} hPa
-        </h1>
-            <h1 className="text-white text-xl lg:text-3xl">
+            <h1 className="text-white text-md lg:text-xl">
               Description: {data.weather[0].description}
             </h1>
           </div>
-          <div className="mt-1 lg:mt-5 lg:mb-5">
+          <div className="mt-4 lg:mt-10">
             <form
               onSubmit={submitData}
               className="flex flex-col lg:flex-row items-center"
             >
               <input
-                className="px-4 py-2 lg:px-10 lg:py-3 rounded-xl mb-2 lg:mb-0 lg:mr-2"
+                className="px-4 py-2 lg:px-10 lg:py-3 rounded-md mb-2 lg:mb-0 lg:mr-2"
                 type="text"
                 value={inputCity}
                 onChange={handleInputChange}
@@ -122,14 +101,13 @@ const App = () => {
               />
               <button
                 type="submit"
-                className="p-2 lg:p-3 bg-slate-900 text-white  rounded-xl"
+                className="p-2 lg:p-3 bg-slate-900 text-white  rounded-md"
               >
                 Get Weather
               </button>
             </form>
           </div>
         </div>
-          </div>
       </div>
     </>
   );
